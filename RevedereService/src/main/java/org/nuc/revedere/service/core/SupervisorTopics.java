58afecdd1aeb4d6d8ee66d58733f442ce00fd270
Nuc Andrei <you_nuc@yahoo.com>
2014-04-17 11:52:37 +0300

    Renamed Topics to match ActiveMQ style

--- RevedereService/src/main/java/org/nuc/revedere/service/core/SupervisorTopics.java
+++ RevedereService/src/main/java/org/nuc/revedere/service/core/SupervisorTopics.java
@@ -3,8 +3,8 @@ package org.nuc.revedere.service.core;
 public class SupervisorTopics {
 
     // Supervisor information related topic
-    public static final String HEARTBEAT_TOPIC = "supervised.heartbeat";
+    public static final String HEARTBEAT_TOPIC = "Revedere.Supervised.Heartbeat";
     
-    public static final String COMMAND_TOPIC = "supervisor.command";
+    public static final String COMMAND_TOPIC = "Revedere.Supervisor.Command";
     
 }
