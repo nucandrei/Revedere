0479e09986e97e69013a5591ee28bea0a216ee23
Andrei Nuc <you_nuc@yahoo.com>
2014-08-15 03:04:00 +0300

    #70 Passwords are now encrypted.

--- Gateway/src/main/java/org/nuc/revedere/gateway/usersmanager/UsersHandler.java
+++ Gateway/src/main/java/org/nuc/revedere/gateway/usersmanager/UsersHandler.java
@@ -7,7 +7,10 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.log4j.Logger;
 import org.jdom2.Document;
 import org.jdom2.Element;
@@ -37,9 +40,9 @@ public class UsersHandler {
     private static final boolean NOT_ADMIN = false;
     private static final String USERS_FILE = "users.xml";
     private static final Logger LOGGER = Logger.getLogger(UsersHandler.class.getName());
-   
 
     private final Map<String, User> users = new HashMap<>();
+    private final Map<User, String> salts = new HashMap<>();
     private final Set<User> connectedUsers = new HashSet<>();
     private final Set<User> disconnectedUsers = new HashSet<>();
     private final Map<LoginRequest, User> usersWaitingAcknowledgement = new HashMap<>();
@@ -60,17 +63,17 @@ public class UsersHandler {
         if (correspondingUser == null) {
             return new Response<>(request, false, USER_DOES_NOT_EXIST);
         }
-        
+
         if (loginSource.equals(LoginRequest.FROM_HEARTMONITOR) && !correspondingUser.isAdmin()) {
             return new Response<>(request, false, ONLY_ADMINS_IN_HEARTMONITOR);
         }
 
-        if (correspondingUser.matchesAuthInfo(authInfo)) {
+        if (correspondingUser.matchesAuthInfo(digestPassword(authInfo, salts.get(correspondingUser)))) {
             LOGGER.info(String.format("Authentification succedded for \"%s\"", username));
             usersWaitingAcknowledgement.put(request, correspondingUser);
             return new Response<>(request, true, AUTH_SUCCESS);
         }
-        
+
         LOGGER.info(String.format("Authentification failed for \"%s\"", username));
         return new Response<>(request, false, AUTH_FAILED);
     }
@@ -101,7 +104,12 @@ public class UsersHandler {
             return new Response<>(request, false, USER_ALREADY_EXISTS);
         }
 
-        users.put(username, new User(username, authInfo, NOT_ADMIN));
+        final String salt = generateSalt();
+        final String hashedPassword = digestPassword(authInfo, salt);
+        final User user = new User(username, hashedPassword, NOT_ADMIN);
+        users.put(username, user);
+        salts.put(user, salt);
+
         saveUsers();
         LOGGER.info(String.format("User \"%s\" registered succesfully", username));
         return new Response<>(request, true, REGISTER_SUCCEDDED);
@@ -115,7 +123,7 @@ public class UsersHandler {
         if (userToUnregister == null) {
             return new Response<>(request, false, USER_DOES_NOT_EXIST);
         }
-        if (userToUnregister.matchesAuthInfo(authInfo)) {
+        if (userToUnregister.matchesAuthInfo(digestPassword(authInfo, salts.get(userToUnregister)))) {
             users.remove(username);
             saveUsers();
             return new Response<>(request, true, UNREGISTER_SUCCEDDED);
@@ -159,8 +167,19 @@ public class UsersHandler {
         for (User user : users.values()) {
             final Element userElement = new Element("user");
             userElement.setAttribute("username", user.getUsername());
-            userElement.setAttribute("authinfo", user.getAuthInfo());
             userElement.setAttribute("isAdmin", user.isAdmin() + "");
+
+            final Element authinfoElement = new Element("digest");
+            authinfoElement.setText(user.getAuthInfo());
+
+            final Element saltElement = new Element("salt");
+            saltElement.setText(salts.get(user));
+
+            final Element securityElement = new Element("security");
+            securityElement.addContent(authinfoElement);
+            securityElement.addContent(saltElement);
+
+            userElement.addContent(securityElement);
             rootElement.addContent(userElement);
         }
 
@@ -185,10 +204,15 @@ public class UsersHandler {
             final Element rootNode = document.getRootElement();
             for (Element e : rootNode.getChildren("user")) {
                 final String username = e.getAttributeValue("username");
-                final String authInfo = e.getAttributeValue("authinfo");
                 final boolean isAdmin = Boolean.parseBoolean(e.getAttributeValue("isAdmin"));
+
+                final Element securityElement = e.getChild("security");
+                final String authInfo = securityElement.getChildText("digest");
+                final String salt = securityElement.getChildText("salt");
+
                 final User newUser = new User(username, authInfo, isAdmin);
                 users.put(username, newUser);
+                salts.put(newUser, salt);
                 disconnectedUsers.add(newUser);
                 LOGGER.info(String.format("Loaded user \"%s\" ", username));
             }
@@ -212,5 +236,13 @@ public class UsersHandler {
 
     private void notifySubscribers() {
         parentManager.sendUpdateToSubscribers(listUsers(null));
+    }
+
+    private String digestPassword(String password, String salt) {
+        return Base64.encodeBase64String(DigestUtils.getSha384Digest().digest((password + salt).getBytes()));
+    }
+
+    private String generateSalt() {
+        return Base64.encodeBase64String(UUID.randomUUID().toString().getBytes());
     }
 }
